e4d4be7e3fe6ccc02d8ec01e9b49803d89e63bdc
Luis Santiago Hermoza Ramos <saluhera2023@gmail.com>
2026-06-25 03:39:29 -0500

    Implementación basica del arbol de busqueda binaria

--- quicklibrary/structures/tree/CustomBSTree.java
+++ quicklibrary/structures/tree/CustomBSTree.java
@@ -1,1 +1,47 @@
+package quicklibrary.structures.tree;
 
+public class CustomBSTree<E extends Comparable<E>> {
+    protected Node root;
+
+    protected class Node {
+        protected E data;
+        protected Node left;
+        protected Node right;
+
+        public Node(E data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public CustomBSTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public int height(Node node) {
+        if (node == null) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+    
+    public E search(E x) {
+        return searchRec(this.root, x);
+    }
+
+    private E searchRec(Node node, E x) {
+        if (node == null) return null;
+        
+        int cmp = x.compareTo(node.data);
+        if (cmp < 0) {
+            return searchRec(node.left, x);
+        } else if (cmp > 0) {
+            return searchRec(node.right, x);
+        } else {
+            return node.data; 
+        }
+    }
+}
