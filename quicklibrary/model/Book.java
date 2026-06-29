88cd7f92d7cdfb7b263402f28d788a46eaddbe44
Jhoel Italo Incahuanaco Yucra <129285104+jhazielg@users.noreply.github.com>
2026-06-29 00:00:30 -0500

    Correct typos in Book class
    Fix typos in Book class attributes and constructor.

---
+++ quicklibrary/model/Book.java
@@ -0,0 +1,13 @@
+public class Book<E> {
+  provate stribg titulo;
+  private strig autor:
+  private E codigo; 
+
+  public Book (Steing titulo, String autor. E codigo){
+    this.tiulo = titulo;
+    this.autor = autor;
+    this.codigo = codigo;
+  
+
+}
+
