5a62757cbec84001c5b2841be454058a730da815
Fabrizzio Ochoa <fbrzzochoa@gmail.com>
2026-06-29 18:25:10 -0500

    Se implementa el comparable para la comparacion de solicitudes y tambien se sobreescribe el metodo compareTo

--- quicklibrary/model/LoanRequest.java
+++ quicklibrary/model/LoanRequest.java
@@ -1,7 +1,7 @@
 package quicklibrary.model;
 
 import java.time.LocalDate;
-public class LoanRequest {
+public class LoanRequest implements Comparable<LoanRequest> {
     private String codigoEstudiante;
     private String nombreEstudiante;
     private String codigoLibro;
@@ -13,4 +13,16 @@ public class LoanRequest {
         this.codigoLibro = codigoLibro;
         this.fechaSolicitud = fechaSolicitud;
     }
+    @Override
+    public int compareTo(LoanRequest otraSolicitud) {
+        if (otraSolicitud == null) {
+            return 1;
+        }
+        int comparacion = this.fechaSolicitud.compareTo(otraSolicitud.fechaSolicitud);
+        if (comparacion != 0) {
+            return comparacion;
+        }
+        return this.codigoEstudiante.compareToIgnoreCase(otraSolicitud.codigoEstudiante);
+    }
+    
 }
