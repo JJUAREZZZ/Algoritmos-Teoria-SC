be41c1a4bc59c7c0a26c4e8d5b4f668e90060b00
Fabrizzio Ochoa <fbrzzochoa@gmail.com>
2026-06-30 07:16:16 -0500

    Complementando la lista enlazada con otros metodos

--- quicklibrary/structures/list/CustomLinkedList.java
+++ quicklibrary/structures/list/CustomLinkedList.java
@@ -61,3 +61,81 @@ public class CustomLinkedList<T extends Comparable<T>> {
         }
         return actual.dato;
     }
+    
+    public boolean remove(T dato) {
+        if (dato == null || isEmpty()) {
+            return false;
+        }
+        if (primero.dato.equals(dato)) {
+            primero = primero.siguiente;
+            cantidad--;
+            if (primero == null) {
+                ultimo = null;
+            }
+            return true;
+        }
+        ListNode<T> anterior = primero;
+        ListNode<T> actual = primero.siguiente;
+        while (actual != null) {
+            if (actual.dato.equals(dato)) {
+                anterior.siguiente = actual.siguiente;
+                if (actual == ultimo) {
+                    ultimo = anterior;
+                }
+                cantidad--;
+                return true;
+            }
+            anterior = actual;
+            actual = actual.siguiente;
+        }
+        return false;
+    }
+
+    public void clear() {
+        primero = null;
+        ultimo = null;
+        cantidad = 0;
+    }
+
+    public void insertOrdered(T dato) {
+        if (dato == null) {
+            return;
+        }
+        ListNode<T> nuevoNodo = new ListNode<T>(dato);
+        if (isEmpty()) {
+            primero = nuevoNodo;
+            ultimo = nuevoNodo;
+            cantidad++;
+            return;
+        }
+        if (((Comparable<T>) dato).compareTo(primero.dato) <= 0) {
+            nuevoNodo.siguiente = primero;
+            primero = nuevoNodo;
+            cantidad++;
+            return;
+        }
+        ListNode<T> actual = primero;
+        while (actual.siguiente != null && ((Comparable<T>) dato).compareTo(actual.siguiente.dato) > 0) {
+            actual = actual.siguiente;
+        }
+        nuevoNodo.siguiente = actual.siguiente;
+        actual.siguiente = nuevoNodo;
+        if (nuevoNodo.siguiente == null) {
+            ultimo = nuevoNodo;
+        }
+        cantidad++;
+    }
+
+    public String mostrarComoTexto() {
+        StringBuilder sb = new StringBuilder();
+        ListNode<T> actual = primero;
+        while (actual != null) {
+            sb.append(actual.dato);
+            if (actual.siguiente != null) {
+                sb.append("\n");
+            }
+            actual = actual.siguiente;
+        }
+        return sb.toString();
+    }
+}
