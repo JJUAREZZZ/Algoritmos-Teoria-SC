da6536081eedb78b87caa1b8f6d36fe952e1d285
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2026-06-29 19:58:58 -0500

    Update BookPanel.java

--- quicklibrary/view/gui/BookPanel.java
+++ quicklibrary/view/gui/BookPanel.java
@@ -1,11 +1,2 @@
 package quicklibrary.view.gui;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.table.*;
-import quicklibrary.controller.LibraryController;
-import quicklibrary.model.Book;
-import quicklibrary.model.BookStatus;
-import quicklibrary.structures.list.CustomLinkedList;
