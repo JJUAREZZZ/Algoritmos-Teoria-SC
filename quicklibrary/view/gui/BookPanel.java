7a4bac64b06c2e45067a7181d7da0bba1c94d13f
JJUAREZZ <juliojuar122006@gmail.com>
2026-06-29 21:29:06 -0500

    añadir una plantilla para luego colocar los metodos cuando se tengan

--- quicklibrary/view/gui/BookPanel.java
+++ quicklibrary/view/gui/BookPanel.java
@@ -1,35 +1,76 @@
 package quicklibrary.view.gui;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.*;
+import quicklibrary.controller.LibraryController;
+import quicklibrary.model.Book;
+import quicklibrary.model.BookStatus;
+import quicklibrary.structures.list.CustomLinkedList;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.table.DefaultTableModel;
-
-public class LoanPanel extends JPanel {
+public class BookPanel extends JPanel {
     private LibraryController controlador;
-    private JTable tablaSolicitudes;
-    private JTable tablaHistorial;
-    private DefaultTableModel modeloSolicitudes;
-    private DefaultTableModel modeloHistorial;
+    private JTable tablaLibros;
+    private DefaultTableModel modeloTabla;
 
-    private JTextField txtCodigoEstudiante;
-    private JTextField txtNombreEstudiante;
-    private JTextField txtCodigoLibro;
+    private JTextField txtCodigo;
+    private JTextField txtTitulo;
+    private JTextField txtAutor;
+    private JTextField txtCategoria;
+    private JTextField txtAnio;
+    private JComboBox<BookStatus> cboEstado;
 
- public LoanPanel(LibraryController controlador) {
+    private JTextField txtBusqueda;
+    private JComboBox<String> cboCriterio;
+
+    public BookPanel(LibraryController controlador) {
         this.controlador = controlador;
-        setLayout(new BorderLayout(10, 10));
-        crearTablas();
+        setLayout(new BorderLayout(15, 15));
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        setBackground(Color.WHITE);
+        crearTabla();
         crearFormulario();
-        actualizarTodo();
     }
+
+    private void crearTabla() {
+    }
+
+    private void crearFormulario() {
+    }
+
+    private JButton crearBotonEstilizado(String texto, Color fondo) {
+        return null;
+    }
+
+    public void actualizarTabla(CustomLinkedList<Book> libros) {
+    }
+
+    private void registrarLibro() {
+    }
+
+    private void modificarLibro() {
+    }
+
+    private void eliminarLibro() {
+    }
+
+    private void buscarPorCodigo() {
+    }
+
+    private void buscarPorTexto() {
+    }
+
+    private void mostrarArbol() {
+    }
+
+    private void cargarSeleccion() {
+    }
+
+    private void limpiarCampos() {
+    }
+
+    private void mostrarError(String mensaje) {
+    }
+}
