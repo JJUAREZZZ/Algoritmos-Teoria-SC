d6019fb9984d0b92340a19180ff4f32148763193
JJUAREZZ <juliojuar122006@gmail.com>
2026-06-29 20:43:42 -0500

    plantilla report panel

--- quicklibrary/view/gui/ReportPanel.java
+++ quicklibrary/view/gui/ReportPanel.java
@@ -5,3 +5,29 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import quicklibrary.controller.LibraryController;
+
+public class ReportPanel extends JPanel {
+    private LibraryController controlador;
+    private JTextArea areaReporte;
+
+    public ReportPanel(LibraryController controlador) {
+        this.controlador = controlador;
+        setLayout(new BorderLayout(15, 15));
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        setBackground(Color.WHITE);
+        crearComponentes();
+    }
+
+    private void crearComponentes() {
+    }
+
+    private JButton crearBotonEstilizado(String texto, Color fondo) {
+        return null;
+    }
+
+    public void actualizarReporte() {
+    }
+
+    private void exportarReporte() {
+    }
+}
