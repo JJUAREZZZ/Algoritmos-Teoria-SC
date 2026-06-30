7ddda27f2b9210c77a66b38c76286e4a1aa95df0
JJUAREZZ <juliojuar122006@gmail.com>
2026-06-29 19:03:05 -0500

    cambio de tipo de protected a public para la interfaz de java swing

--- quicklibrary/structures/tree/CustomAVLTree.java
+++ quicklibrary/structures/tree/CustomAVLTree.java
@@ -2,7 +2,7 @@ public class CustomAVLTree<E extends Comparable<E>> extends CustomBSTree<E> {
     private boolean height;
 
     protected class NodeAVL extends Node {
-        protected int bf;
+        public int bf;
 
         public NodeAVL(E data) {
             super(data);
