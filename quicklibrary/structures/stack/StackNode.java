cd4ca335a90202d64a967a7a135019ec60c15142
Luis Santiago Hermoza Ramos <saluhera2023@gmail.com>
2026-06-30 06:21:20 -0500

    Creacion del Nodo de la Pila

--- quicklibrary/structures/stack/StackNode.java
+++ quicklibrary/structures/stack/StackNode.java
@@ -1,1 +1,11 @@
+package quicklibrary.structures.stack;
 
+class StackNode<T> {
+    T dato;
+    StackNode<T> siguiente;
+
+    StackNode(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
