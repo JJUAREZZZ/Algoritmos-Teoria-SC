902055c99f94729c6a3373bb70c1df6b1b09a563
Luis Santiago Hermoza Ramos <saluhera2023@gmail.com>
2026-06-30 06:29:38 -0500

    Creación de un nodo para listas enlazadas

--- quicklibrary/structures/list/ListNode.java
+++ quicklibrary/structures/list/ListNode.java
@@ -1,2 +1,11 @@
 package quicklibrary.structures.list;
 
+class ListNode<T> {
+    T dato;
+    ListNode<T> siguiente;
+
+    ListNode(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
