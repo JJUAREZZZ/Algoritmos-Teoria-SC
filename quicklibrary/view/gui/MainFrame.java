a6a18b14e6c88f9cb13f274d98cd220082b7c763
JJUAREZZ <juliojuar122006@gmail.com>
2026-06-29 20:43:25 -0500

    plantilla main frame

--- quicklibrary/view/gui/MainFrame.java
+++ quicklibrary/view/gui/MainFrame.java
@@ -4,3 +4,23 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import quicklibrary.controller.LibraryController;
+
+public class MainFrame extends JFrame {
+    private LibraryController controlador;
+    private BookPanel bookPanel;
+    private LoanPanel loanPanel;
+    private ReportPanel reportPanel;
+    private FilePanel filePanel;
+
+    public MainFrame() {
+        this.controlador = new LibraryController();
+        configurarVentana();
+        crearComponentes();
+    }
+
+    private void configurarVentana() {
+    }
+
+    private void crearComponentes() {
+    }
+}
