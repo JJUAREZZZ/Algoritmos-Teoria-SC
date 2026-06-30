c2616f2cf8494bd786591f1b1fa6af4f69f8a3c3
JJUAREZZ <juliojuar122006@gmail.com>
2026-06-29 20:25:54 -0500

    actualizar file panel con constructor

--- quicklibrary/view/gui/FilePanel.java
+++ quicklibrary/view/gui/FilePanel.java
@@ -7,3 +7,15 @@ import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.filechooser.*;
 import quicklibrary.controller.LibraryController;
+
+public class FilePanel extends JPanel {
+    private LibraryController controlador;
+    private JTextArea areaLog;
+
+    public FilePanel(LibraryController controlador) {
+        this.controlador = controlador;
+        setLayout(new BorderLayout(15, 15));
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        setBackground(Color.WHITE);
+        crearComponentes();
+    }
